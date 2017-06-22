cb3fd1219f9f298db4b39f2e2a6af0f0176de05d
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2017-06-22 16:35:30 +0530

    chanfe the code to update immediately

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,5 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-$(window).on('mousedown', mouseDown);
-$(window).on('mouseup', mouseUp);
-$(window).on('mousemove', mouseMove);
 
 var colors = {
   red: '#ff4436',
@@ -10,14 +7,18 @@ var colors = {
   blue: '#3F51B5'
 }
 
-var objects = [];    
 var mousedown = false;
-var x = 0, y = 0, radius = 2;
 var color = colors.red;
 var prevX = -1, prevY = -1;
-var object = {};
-var pts = [];
 
+$(window).on('mousedown', mouseDown);
+$(window).on('mouseup', mouseUp);
+$(window).on('mousemove', mouseMove);
+
+
+$(document).ready(function() {
+
+});
 $('.color').on('click', function() {
     $('#red').css('border-color', 'transparent');
     $('#green').css('border-color', 'transparent');
@@ -29,13 +30,7 @@ $('.color').on('click', function() {
 var socket = io();
 
 socket.on('draw', function(obj) {
-  var pts = obj.points;
-  drawLine(obj.startX, obj.startY, obj.color);
-  for(var i = 0; i < pts.length; i ++)
-  {
-     lines(pts[i].x, pts[i].y);
-  } 
-  ctx.closePath();
+  drawLine(obj.startX, obj.startY, obj.endX, obj.endY, obj.color);
 });
 
 /* Init */
@@ -46,44 +41,26 @@ socket.on('draw', function(obj) {
 })();
     
 function draw(x, y)
-{
+{   
     if(prevX != -1 && prevY != -1)
     {
-      drawLine(prevX, prevY, color);
-      lines(x, y);
-      ctx.closePath();
-      pts.push({x: x, y: y});
-      prevX = x;
-      prevY = y;
-
+        drawLine(prevX, prevY, x, y, color);
+        socket.emit('draw', {startX: prevX, startY: prevY, endX: x, endY: y, color: color});
     }
-    else if(prevX == -1 && prevY == -1)
-    {
-      object.startX = x;
-      object.startY = y;
-      object.color = color;
-      prevX = x;
-      prevY = y;
-    }
+    prevX = x;
+    prevY = y;
 }
 
 /* Listener Callbacks */
 function mouseDown(e)
 {
   mousedown = true;
-  x = e.clientX;
-  y = e.clientY;
 }
 
 function mouseUp(e)
 {
-    object.points = pts;
-    objects.push(object);
-    socket.emit('draw', object);
     prevX = -1;
-    prevY = -1; 
-    object = {};
-    pts = [];
+    prevY = -1;
     mousedown = false;
 }
 
@@ -96,16 +73,12 @@ function mouseMove(e) {
 
 
 /* Drawing components */
-function drawLine(prevX, prevY, color)
+function drawLine(prevX, prevY, x, y, color)
 {
   ctx.strokeStyle = color;
   ctx.lineWidth = 2;
   ctx.beginPath();
   ctx.moveTo(prevX, prevY);
-}
-
-function lines(x, y)
-{
   ctx.lineTo(x, y);
   ctx.stroke();
 }
